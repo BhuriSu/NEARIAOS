a5ab7cb6c31e75be5397e7db32b29877677b4d7e
bhuri supi <spryanun@gmail.com>
2023-04-27 21:13:10 +0700

    debug pool

--- backend/models/modelProfile.js
+++ backend/models/modelProfile.js
@@ -1,4 +1,5 @@
-import { Pool } from 'pg';
+import pkg from 'pg';
+const { Pool } = pkg;
 
 const pool = new Pool({
   user: process.env.DB_USER,
@@ -39,3 +40,5 @@ const createProfileTable = async () => {
 };
 
 createProfileTable();
+
+export { pool };
