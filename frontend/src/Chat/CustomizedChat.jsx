b0d1881ddcdc219dc3260fef676b356aea7a6690
bhuri supi <spryanun@gmail.com>
2023-11-11 02:38:40 +0700

    add chat system

--- frontend/src/Chat/CustomizedChat.jsx
+++ frontend/src/Chat/CustomizedChat.jsx
@@ -1,49 +1,68 @@
-import React, {useState} from 'react'; 
-import { ChannelList, Channel, ChannelSettings } from '@sendbird/uikit-react/';
+import React, { useState, useEffect } from 'react';
+
+const UserProfile = ({ user }) => {
+  return (
+    <div>
+      <img src={user.photo} alt="User Avatar" style={{ width: '50px', height: '50px', borderRadius: '50%' }} />
+      <div>Username: {user.username}</div>
+      <div>Date of Birth: {user.dob}</div>
+      <div>Beverage: {user.beverage}</div>
+      <div>Workplace: {user.workplace}</div>
+      <div>Favorite: {user.favorite}</div>
+      <div>About: {user.about}</div>
+    </div>
+  );
+};
 
 const CustomizedChat = () => {
-    const [currentChannel, setCurrentChannel] = useState(null);
-    const currentChannelUrl = currentChannel ? currentChannel.url : "";
-    const [showSettings, setShowSettings] = useState(false);
-    let channelChatDiv = document.getElementsByClassName("channel-chat")[0];
-    const renderSettingsBar = () => {
-        channelChatDiv.style.width = "52%";
-        channelChatDiv.style.cssFloat = "left";
-    }
-    const hideSettingsBar = () => {
-        channelChatDiv.style.width = "76%";
-        channelChatDiv.style.cssFloat = "right";
-    }
-    return (
-        <div className="channel-wrap">
-            <div className="channel-list">
-                <ChannelList
-                onChannelSelect={(channel)=>{
-                setCurrentChannel(channel);
-                }}    
-                />
-            </div>
-            <div className="channel-chat">
-                <Channel
-                 channelUrl={currentChannelUrl}
-                 onChatHeaderChannelClick={()=>{
-                    setShowSettings(!showSettings);
-                    renderSettingsBar();
-                    }}   
-                />
-            </div>
-            {showSettings && (
-                <div className="channel-settings">
-                <ChannelSettings
-                channelUrl={currentChannelUrl}
-                onCloseClick = {() =>{
-                    setShowSettings(false);
-                    hideSettingsBar();
-                }}
-                />
-                </div>
-            )}
-        </div>
-    );
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
+  const [currentUser, ] = useState({
+    photo: './images/DefaultAvatar.png', // You can set a default avatar image
+    username: 'JohnDoe',
+    dob: '1990-01-01',
+    beverage: 'Coffee',
+    workplace: 'Company XYZ',
+    favorite: 'Movies',
+    about: 'Hello, I am John Doe!',
+  });
+
+  // Simulate a simple conversation
+  useEffect(() => {
+    setMessages([
+      { id: 1, text: 'Hello there!' },
+      { id: 2, text: 'How are you?' },
+    ]);
+  }, []);
+
+  const handleInputChange = (e) => {
+    setNewMessage(e.target.value);
+  };
+
+  const handleSendMessage = () => {
+    if (newMessage.trim() === '') return;
+
+    const newMessages = [...messages, { id: messages.length + 1, text: newMessage }];
+    setMessages(newMessages);
+    setNewMessage('');
+  };
+
+  return (
+    <div>
+      <UserProfile user={currentUser} />
+
+      <div style={{ border: '1px solid #ccc', height: '300px', overflowY: 'scroll' }}>
+        {messages.map((message) => (
+          <div key={message.id}>{message.text}</div>
+        ))}
+      </div>
+
+      <div>
+        <input type="text" value={newMessage} onChange={handleInputChange} />
+        <button onClick={handleSendMessage}>Send</button>
+      </div>
+    </div>
+  );
 };
+
 export default CustomizedChat;
--- frontend/src/Chat/index.jsx
+++ frontend/src/Chat/index.jsx
@@ -1,23 +1,11 @@
 import React from 'react';
-import SendbirdProvider from "@sendbird/uikit-react/SendbirdProvider";
-import "@sendbird/uikit-react/dist/index.css";
-import './ChatElements.css';
 import CustomizedChat from './CustomizedChat';
 
-const Chat = ({username,Photo}) => {
-
+const Chat = () => {
     return (
-        <div className="Chat">
-        <SendbirdProvider
-          appId={import.meta.env.APP_ID}
-          userId={import.meta.env.USER_ID}
-          nickname={username}
-          profileUrl={Photo}
-          allowProfileEdit={true}
-        >
-        <CustomizedChat/>
-        </SendbirdProvider>
-        </div>
+      <div>
+      <CustomizedChat />
+    </div>
     );
 };
 export default Chat;
